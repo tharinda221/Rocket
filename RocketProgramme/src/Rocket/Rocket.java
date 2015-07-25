62ea3e968109e06aa7a025434ed83f731c8a6f57
Thisaru Guruge <sachin.tg@gmail.com>
2015-07-25 12:21:58 +0530

    Create Rocket.java
    Rocket Class Added

---
+++ RocketProgramme/src/Rocket/Rocket.java
@@ -0,0 +1,32 @@
+package rocket;
+
+public abstract class Rocket {
+    private String name, ID;
+    private static int ROCKET_COUNT = 0;
+    
+    public Rocket (String name) {
+        this.name = name;
+        this.ID = "NASA" + ROCKET_COUNT;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    public String getID() {
+        return ID;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+    
+    public abstract void assignPilot(String pilotID);
+    public abstract void fuelEfficiency();
+    
+    public void upCount() {
+        ROCKET_COUNT++;
+    }
+}
