162d63c8f1bc2b519d1dd2c4df49ffc05a747800
Tharinda Ehelepola <tharinda221@gmail.com>
2015-07-25 12:09:40 +0530

    Pilot

--- RocketProgramme/src/Rocket/Pilot.java
+++ RocketProgramme/src/Rocket/Pilot.java
@@ -3,5 +3,25 @@ package Rocket;
 /**
  * Created by Ehelepola on 25/07/2015.
  */
-public class Pilot {
+abstract class Pilot {
+
+    String name;
+    int id;
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public int getId(){
+        return id;
+    }
+    public void show(){
+        System.out.println(name+" "+id);
+    }
 }
